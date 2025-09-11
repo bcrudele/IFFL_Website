3e8c43c56c7c09fcc597554dc506736e0ae6f875
bcrudele <bcrudele@purdue.edu>
2025-09-11 13:39:13 -0500

    Week 1 Duction

--- team-records.js
+++ team-records.js
@@ -1,7 +1,7 @@
 // team-records.js
 const teamRecords = {
-    charge: "0 - 0 - 0",
-    assassins: "0 - 0 - 0",
-    demons: "0 - 0 - 0",
-    turtles: "0 - 0 - 0",
+    charge: "1 - 0 - 0",
+    assassins: "0 - 1 - 0",
+    demons: "0 - 1 - 0",
+    turtles: "1 - 0 - 0",
 };
