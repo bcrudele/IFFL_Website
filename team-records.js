fe63546c1363a795dd32b03746be6560eb0f0359
bcrudele <bcrudele@purdue.edu>
2025-09-16 16:39:26 -0500

    Week 2 Update

--- team-records.js
+++ team-records.js
@@ -1,7 +1,12 @@
 // team-records.js
 const teamRecords = {
-    charge: "1 - 0 - 0",
-    assassins: "0 - 1 - 0",
-    demons: "0 - 1 - 0",
-    turtles: "1 - 0 - 0",
+    charge: "1 - 1 - 0",
+    assassins: "0 - 2 - 0",
+    demons: "1 - 1 - 0",
+    turtles: "2 - 0 - 0",
 };
+
+// demons --> goblins
+// turtles --> beers
+// assassins --> hounds
+// charge --> redskins
